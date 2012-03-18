7c771c05c989a1c01342f2b93baec09072899378
Griffin Boyce <griffinboyce@gmail.com>
2012-03-18 11:31:44 -0400

    Initial file commit

---
+++ handle-zip.php
@@ -0,0 +1,33 @@
+<?php
+	header('Content-type: text/xml');
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+
+	echo '<Response>';
+
+	$zip = $_REQUEST['Digits'];
+
+	if (strlen($zip) == 5)
+	{
+		$db = new PDO('sqlite:ivr.sqlite');
+
+		$stmt = $db->prepare('SELECT address FROM packages WHERE zip=?');
+		$stmt->execute(array($zip));
+		
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if ($result)
+		{
+			echo '<Say>Your closest foodbank is '.$result['address'].'.</Say>';
+		}
+		else {
+			echo "<Say>Sorry, we don't have record of a food bank in that zip code. Please try again or call Philabundance at 215.339.0900.</Say>";
+			echo '<Redirect method="GET">handle-user-input.php?Digits=3</Redirect>';
+		}
+	}
+	else {
+		echo "<Say>Sorry, we don't recognize that zip code. Please try again or call Philabundance at 215.339.0900.</Say>";
+		echo '<Redirect method="GET">handle-user-input.php?Digits=3</Redirect>';
+	}
+
+	echo '</Response>';
+?>
